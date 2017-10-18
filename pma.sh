7fad45f4b4066fcdcb632352432fcb479ae6dd57
mateusz-sawit <biuro@sawit.pl>
2017-10-18 11:32:09 +0200

    definiowanie adresu phpmyadmin przeniesione do homestad.yaml

--- pma.sh
+++ pma.sh
@@ -6,9 +6,3 @@ curl -#L https://files.phpmyadmin.net/phpMyAdmin/4.7.4/phpMyAdmin-4.7.4-english.
 mkdir phpmyadmin && tar xf phpmyadmin.tar.gz -C phpmyadmin --strip-components 1
 
 rm phpmyadmin.tar.gz
-
-CMD=serve
-
-sudo bash $CMD phpmyadmin.app $(pwd)/phpmyadmin
-
-sudo service nginx reload
